4ab9625ac03ceabf55dfaaa38916608fe0a10eed
calinvladth <calinvladth@icloud.com>
2024-08-26 01:04:58 +0300

    fix root redirect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
   import "@/app.css";
   import Alert from "../components/alert.svelte";
-  import { onMount } from "svelte";
-  import { ROUTES } from "../utils/constants";
-
-  onMount(() => {
-    goto(ROUTES.LOGIN);
-  });
 </script>
 
 <Alert />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+  import { pb } from "../services/pb";
+  import { ROUTES } from "../utils/constants";
+
+  onMount(() => {
+    if (pb.authStore.isValid) {
+      goto(ROUTES.MAPS);
+    } else {
+      goto(ROUTES.LOGIN);
+    }
+  });
+</script>
